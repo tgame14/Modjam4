497d31efee6f4223da475975370dc403e2a8d224
tgame14 <tgameBusiness@gmail.com>
2014-05-15 09:59:09 +0300

    Adding proxies and modmetadata

---
+++ src/main/scala/com/tgame/modjam4/ClientProxy.java
@@ -0,0 +1,9 @@
+package com.tgame.modjam4;
+
+/**
+ * @since 15/05/14
+ * @author tgame14
+ */
+public class ClientProxy extends CommonProxy
+{
+}
---
+++ src/main/scala/com/tgame/modjam4/CommonProxy.java
@@ -0,0 +1,23 @@
+package com.tgame.modjam4;
+
+/**
+ * @since 15/05/14
+ * @author tgame14
+ */
+public class CommonProxy
+{
+    public void preInit()
+    {
+
+    }
+
+    public void init()
+    {
+
+    }
+
+    public void postInit()
+    {
+
+    }
+}
